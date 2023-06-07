1e847466020eb848dc43a544628300d34e53f8af
Bartosz Stachowiak <sbartekt@op.pl>
2023-06-07 22:17:18 +0200

    pass example dir to trainer initializer

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pathlib
 from trainer.application import Trainer
 
 if __name__ == "__main__":
-    app = Trainer()
+    app = Trainer(pathlib.Path("UTPackages/Example").resolve())
     app.run()
